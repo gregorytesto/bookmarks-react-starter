2e0237552668c74705515a482a0cfd563a84e1c7
Greg Testo <gregt@pursuit.org>
2022-01-13 13:12:04 -0400

    end of part one

--- src/Components/Bookmarks.js
+++ src/Components/Bookmarks.js
@@ -1,8 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Bookmark from "./Bookmark";
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 function Bookmarks() {
-  const [bookmarks] = useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
+  useEffect(()=>{
+    // http://www.localhost:3003/bookmarks
+    // axios.get(API_URL + "/bookmarks")
+    //   .then((res)=>{
+    //     setBookmarks(res.data);
+    //   }).catch((err)=>{
+    //     throw err;
+    //   });
+    
+
+    fetch(API_URL + "/bookmarks")
+      .then((res)=>{
+        return res.json();
+      }).then((data)=>{
+        setBookmarks(data);
+      }).catch((err)=> {
+        throw err
+      });
+  }, []);
+
   return (
     <div className="Bookmarks">
       <section>
